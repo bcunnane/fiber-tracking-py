e625f824efbd31f3ddf4dcc9ea2d25d1859bc47c
bcunnane <brandon.cunnane@gmail.com>
2022-09-15 15:00:12 -0700

    converted rslt to dataframe

--- fiber_track_analysis.py
+++ fiber_track_analysis.py
@@ -1,70 +1,109 @@
 import shelve
 import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.api import qqplot
-import scipy.stats as stats
 import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy.stats as stats
 
 
-def unshelve_data():
+
+def import_data():
+    # unshelve raw data
     shelf_file = shelve.open('fiber-track-data-py')
-    fiber_track_data = shelf_file['data']
+    data = shelf_file['data']
     shelf_file.close()
-    return fiber_track_data
-
-# %% calculations
-data = unshelve_data()
-
-pix_spacing = 1.1719
-
-rslt = {'force':data['force'], 'id':data['id'], 'mvc':data['mvc']}
-dxs = data['xs'][:,1::2,:] - data['xs'][:,0::2,:] 
-dys = data['ys'][:,1::2,:] - data['ys'][:,0::2,:]
-rslt['lengths'] = ((dxs**2 + dys**2) ** .5) * pix_spacing
-del_lens = rslt['lengths'] - rslt['lengths'][:,:,0][:,:,np.newaxis]
-rslt['angles'] = np.cos(np.absolute(dys / rslt['lengths']))
-rslt['strains'] = del_lens / rslt['lengths'][:,:,0][:,:,np.newaxis]
-rslt['angles'] = np.degrees(rslt['angles'])
-
-rslt['peak_strain'] = np.min(rslt['strains'], axis=2)
-rslt['ps_idx'] = np.argmin(rslt['strains'], axis=2)
-
-rslt['torque'] = rslt['force'] * data['ma'] * 0.001 # Nm
-rslt['mvc_torque'] = rslt['mvc'] * data['ma'] * 0.001 # Nm
-rslt['str/force'] = rslt['peak_strain'] / rslt['force'][:, np.newaxis]
-rslt['str/torque'] = rslt['peak_strain'] / rslt['torque'][:, np.newaxis]
-
-rslt['init_lens'] = rslt['lengths'][:,:,0]
-rslt['init_angs'] = rslt['angles'][:,:,0]
-
-rslt['final_lens'] = np.zeros((36,3))
-rslt['final_angs'] = np.zeros((36,3))
-for n in range(len(rslt['id'])):
-    rslt['final_lens'][n,:] = rslt['lengths'][n,[0,1,2],rslt['ps_idx'][n,:]]
-    rslt['final_angs'][n,:] = rslt['angles'][n,[0,1,2],rslt['ps_idx'][n,:]]
-
-# %% normality check
-fields = ['peak_strain','str/force','str/torque',
-          'init_angs','final_angs','init_lens','final_lens']
-
-p_sw = pd.DataFrame(['D50','N50','P50','D25','N25','P25'], columns=['case'])
-p_sw = p_sw.set_index('case')
-
-for field in fields:
-    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15,10))
-    ax= axes.flatten()
-    p_sw[field] = np.zeros(6)
-    for c in range(6):
-        # get case data
-        rows = [i for i in range(c,36,6)]
-        case_data = rslt[field][rows,:].flatten()
-        
-        # shaprio-wilks test p-value
-        shapiro_rslt = stats.shapiro(case_data)
-        p_sw[field][c] = shapiro_rslt.pvalue
-        
-        # qq plots
-        qqplot(case_data, line='s',ax = ax[c])
-        ax[c].title.set_text(p_sw.index[c])
     
+    # change in fiber endpoint position
+    dxs = data['xs'][:,1::2,:] - data['xs'][:,0::2,:]
+    dys = data['ys'][:,1::2,:] - data['ys'][:,0::2,:]
+    
+    # fiber lengths (lens)
+    pix_spacing = 1.1719 #mm/pixel
+    data['lens'] = ((dxs**2 + dys**2) ** .5) * pix_spacing
+    del_lens = data['lens'] - data['lens'][:,:,0][:,:,None]
+    
+    # fiber strains and peak strain (ps) index
+    data['strains'] = del_lens / data['lens'][:,:,0][:,:,None]
+    data['ps_idx'] = np.argmin(data['strains'], axis=2)
+    
+    # fiber pennation angles (angs, to +y axis in image)
+    data['angs'] = np.cos(np.absolute(dys / data['lens']))
+    data['angs'] = np.degrees(data['angs'])
+    
+    # torque (force * moment arm, ma)
+    data['torque'] = data['force'] * data['ma'] * 0.001 # Nm
+    data['mvc_torque'] = data['mvc'] * data['ma'] * 0.001 # Nm
+    
+    return data
+
+
+def interpret_data(data):
+    # strain and normalized strain
+    peak_str = np.min(data['strains'], axis=2)
+    str_force = peak_str / data['force'][:, None]
+    str_torque = peak_str / data['torque'][:, None]
+    
+    # fiber at rest
+    init_lens = data['lens'][:,:,0]
+    init_angs = data['angs'][:,:,0]
+    
+    # fiber at peak contraction
+    final_lens = np.zeros((36,3))
+    final_angs = np.zeros((36,3))
+    for n in range(len(data['id'])):
+        final_lens[n,:] = data['lens'][n,[0,1,2],data['ps_idx'][n,:]]
+        final_angs[n,:] = data['angs'][n,[0,1,2],data['ps_idx'][n,:]]
+    
+    # collect results into data frame
+    rslt = pd.DataFrame({
+        'posn' : list('DDDDDDNNNNNNPPPPPP' * 6),
+        'pct_mvc':np.tile([50,50,50,25,25,25],(18,)),
+        'peak_str':peak_str.flatten(),
+        'str/force':str_force.flatten(),
+        'str/torque':str_torque.flatten(),
+        'init_lens':init_lens.flatten(),
+        'final_lens':final_lens.flatten(),
+        'init_angs':init_angs.flatten(),
+        'final_angs':final_angs.flatten(),
+        })
+    return rslt
+
+
+def check_normality(rslt):
+    
+    p_sw = pd.DataFrame({'case':['D50','N50','P50','D25','N25','P25']})
+    
+    for field in rslt.columns[2:]:
+        fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15,10))
+        ax= axes.flatten()
+        p_sw[field] = np.zeros(6)
+        c = 0
+        for pn in 'DNP':
+            for mvc in [50,25]:
+            # get c data
+                case_idx = (rslt['posn']==pn) & (rslt['pct_mvc']==mvc)
+                case_data = rslt[field][case_idx]
+                
+                # shaprio-wilks test p-value
+                shapiro_rslt = stats.shapiro(case_data)
+                p_sw.loc[c, field] = shapiro_rslt.pvalue
+                
+                # qq plots
+                sm.qqplot(case_data, line='s',ax = ax[c])
+                ax[c].title.set_text(p_sw.loc[c, 'case'])
+                c += 1
+    return p_sw
+
+
+
+
+#def main():
+data = import_data()
+rslt = interpret_data(data)
+p_sw = check_normality(rslt)
+
+#def stats_norm(rslt):
+
+
+
 
